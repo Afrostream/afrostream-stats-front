633942e932021766baa39b148a483f79e46c53a9
spebsd <spebsd@gmail.com>
2016-04-08 16:19:18 +0200

    Remove unecessary files

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,16 +3,7 @@ import {render} from 'react-dom'
 import UsersStatsPageComponent from './UsersStatsPageComponent.jsx'
 import ASNumberStatsPageComponent from './ASNumberStatsPageComponent.jsx'
 import { Router, Route, browserHistory } from 'react-router'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import cloudBunkrApp from './reducers/Reducers.jsx'
-import { UISignupVisibilityFilter, setUISignupVisibility } from './actions/Actions.jsx'
-
-const store = createStore(cloudBunkrApp);
-let unsubscribe = store.subscribe(() =>
-    console.log(store.getState())
-);
-store.dispatch(setUISignupVisibility(UISignupVisibilityFilter.SHOW));
 
 class App extends React.Component {
   render() {
